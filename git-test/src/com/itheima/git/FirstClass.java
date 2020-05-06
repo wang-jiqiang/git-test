54fd7fdec8e55d507f3a3e85e2d1c9bf167c8f32
wang-jiqiang <1317742703@qq.com>
2020-05-06 17:53:08 +0800

    本地工程添加到git中

---
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @author wangjiqiang
+ * @version 1.0
+ * @date 2020/5/6 0006 17:38
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.print("hello git project");
+        System.out.print("hello git project"); System.out.print("hello git project"); System.out.print("hello git project"); System.out.print("hello git project");
+        System.out.print("hello git project");
+
+
+
+    }
+}
