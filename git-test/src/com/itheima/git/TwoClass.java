fea717d836cf11cf27fdf6367aa7b93fd17eb6e4
wang-jiqiang <1317742703@qq.com>
2020-05-07 09:27:17 +0800

    这是分支做的

---
+++ git-test/src/com/itheima/git/TwoClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @author wangjiqiang
+ * @version 1.0
+ * @date 2020/5/7 0007 09:24
+ */
+public class TwoClass {
+    public static void main(String[] args) {
+        System.out.println("WOT");
+    }
+}
